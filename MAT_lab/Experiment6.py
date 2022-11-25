35bf17b7c67d8a2a184323e9c4998be3e7c119f1
KUNAL SHARMA <kunal03b@gmail.com>
2022-11-25 11:05:13 +0530

    final

--- MAT_lab/Experiment6.py
+++ MAT_lab/Experiment6.py
@@ -19,3 +19,20 @@ plt.xlabel("t_values")
 plt.ylabel("probability for t_values<=t")
 plt.title("  CDF for t distribution with df = 20")
 plt.show()
+
+
+# ------------------------------Moment Generating Functions ------------------------------
+
+from scipy import stats
+data = [[3, 4, 6, 7, 9],
+       [21, 34, 43, 2, 67],
+       [4, 7, 5, 27, 1]]
+print("")
+print("Moment Generating Function")
+print("0th Moment=", stats.moment(data, moment=0,axis=1))
+print("1st  Moment=", stats.moment(data, moment=1,axis=1))
+print("2nd  Moment=", stats.moment(data, moment=2,axis=1))
+print("3rd  Moment=", stats.moment(data, moment=3,axis=1))
+print("4th  Moment=", stats.moment(data, moment=4,axis=1))
+print("5th  Moment=", stats.moment(data, moment=5,axis=1))
+
