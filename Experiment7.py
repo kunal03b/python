5604305d231256284e83bb51d19fdbd735aaffba
KUNAL SHARMA <kunal03b@gmail.com>
2022-11-25 11:44:36 +0530

    updated

--- Experiment7.py
+++ Experiment7.py
@@ -1,1 +1,27 @@
 # Implementation of database connectivity and perform CRUD
+
+import sqlite3
+conn = sqlite3.connect('test.db')
+print("Opened database successfully")
+query = """CREATE TABLE STUDENT2(ROLLNO INT PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, 
+CLASS INT NOT NULL, ROOM INT NOT NULL)"""
+conn.execute(query)
+print("Table created successfully")
+# Step-2: Inserting into a Table
+query = """ INSERT INTO STUDENT2(ROLLNO,NAME,CLASS,ROOM) VALUES (11212505, 'PUNIT',  'BTECH', 144 ) """
+conn.execute(query)
+conn.commit()
+print("Records created successfully")
+# Step-3: Selection/Retrieve data from a Database
+cursor = conn.execute("SELECT ROLLNO, name, CLASS, ROOM from STUDENT2")
+for row in cursor:
+    print("ROLLNO = 11212505 ", row[0])
+print("NAME = PUNIT ", row[1])
+print("CLASS = BTECH ", row[2])
+print("ROOM = 144 ", row[3])
+print("Operation done successfully")
+#conn.close()
+# Step-4: Deletion from Table:
+cursor = conn.execute("DELETE FROM STUDENT2 WHERE ROLLNO = 11212505")
+print("Record delete successfully")
+conn.close()
