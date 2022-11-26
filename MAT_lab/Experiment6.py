0d3d71d5f0662e5b1313e6b898357d671dbae142
KUNAL SHARMA <kunal03b@gmail.com>
2022-11-26 16:53:24 +0530

    updated

--- MAT_lab/Experiment6.py
+++ MAT_lab/Experiment6.py
@@ -35,4 +35,3 @@ print("2nd  Moment=", stats.moment(data, moment=2,axis=1))
 print("3rd  Moment=", stats.moment(data, moment=3,axis=1))
 print("4th  Moment=", stats.moment(data, moment=4,axis=1))
 print("5th  Moment=", stats.moment(data, moment=5,axis=1))
-
